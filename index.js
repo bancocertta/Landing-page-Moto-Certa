55fdc8d50df783b621cd4d594fcee84bd80dc594
rodr_ps <rodrigospinheiro22@gmail.com>
2023-10-10 13:31:02 -0300

    atualização de vendedores

--- index.js
+++ index.js
@@ -3,12 +3,37 @@ var vendedor
 var num
 
 if(sorteio==0){
-    num = "5584996238690"
-    vendedor = " Romário"
+    num = "5584981053495"
 }
 else if(sorteio==1){
-    num = "5584988602680"
-    vendedor = " Allana"
+    num = "5584981053495"
 }
 
-document.getElementById("link").href = "https://wa.me/" + num + "?text=Olá," + vendedor;
+document.getElementById("link").href = "https://wa.me/" + num + "?text=Olá";
+
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -5.808470626824163, lng:  -35.21643543981361};
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 15,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
